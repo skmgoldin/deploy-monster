052c0c48309be1ed6ba4c9fe1f7f13da5a19d142
Mike Goldin <mike.goldin@consensys.net>
2016-12-21 19:30:12 -0800

    Add test for output capture

--- tests/tests.js
+++ tests/tests.js
@@ -10,8 +10,10 @@ describe('index.js', function() {
     let opts
     let testDef
     let testInstance
+    let output
 
     this.timeout(5000)
+
     before(function(done) {
       web3 = new Web3()
       web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'))
@@ -28,25 +30,29 @@ describe('index.js', function() {
           txParams: txParams,
           web3: web3
         }
-        dm.compileAndDeploy(opts).then(function(output) {
-          testDef = web3.eth.contract(output.Test.abi);
-          testInstance = testDef.at(output.Test.address);
+        dm.compileAndDeploy(opts).then(function(_output) {
+          output = _output
+          testDef = web3.eth.contract(output.Test.abi)
+          testInstance = testDef.at(output.Test.address)
           done()
         })
       })
     })
 
-    it('should deploy the test.sol contract', function(done) {
-        chai.assert.equal(testInstance.sender(), accts[0]);
-        chai.assert.equal(testInstance.addr(), accts[1]);
-        chai.assert.equal(testInstance.number().c[0], 4);
-        chai.assert.equal(testInstance.boolean(), true);
-        done();
-      })
-    it('should capture the address of the test.sol contract')
-    it('should capture the abi of the test.sol contract')
-    it('should capture the txHash of the test.sol contract')
-    it('should capture the bytecode of the test.sol contract')
+    it('should deploy the test.sol contract', function() {
+      chai.assert.equal(testInstance.sender(), accts[0])
+      chai.assert.equal(testInstance.addr(), accts[1])
+      chai.assert.equal(testInstance.number().c[0], 4)
+      chai.assert.equal(testInstance.boolean(), true)
+    })
+    it('should capture the address, abi, txHash and bytecode of the test.sol contract',
+        function() {
+      chai.assert.typeOf(output.Test.address, 'string')
+      chai.assert.typeOf(output.Test.abi, 'array')
+      chai.assert.typeOf(output.Test.txHash, 'string')
+      chai.assert.typeOf(output.Test.bytecode, 'string')
+
+    })
   })
   describe('#compileAndDeployFromConfig(configPath)', function() {
     it('should deploy the test.sol contract')
