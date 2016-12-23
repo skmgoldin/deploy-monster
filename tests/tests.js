44cad78854a38de9a7075e5be58c91fc4a54b472
Mike Goldin <mike.goldin@consensys.net>
2016-12-23 13:18:36 -0800

    Stub in full desired test suite

--- tests/tests.js
+++ tests/tests.js
@@ -3,7 +3,7 @@ const dm = require('../lib/index.js')
 const Web3 = require('Web3');
 
 describe('index.js', function() {
-  describe('#compileAndDeploy(opts)', function() {
+  describe('using a single contract with no dependencies...', function() {
     let web3
     let accts
     let txParams
@@ -14,7 +14,7 @@ describe('index.js', function() {
 
     this.timeout(5000)
 
-    before(function(done) {
+    function setup(cb) {
       web3 = new Web3()
       web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'))
       web3.eth.getAccounts(function(err, _accts) {
@@ -34,30 +34,86 @@ describe('index.js', function() {
           output = _output
           testDef = web3.eth.contract(output.Test.abi)
           testInstance = testDef.at(output.Test.address)
-          done()
+          cb()
         })
       })
-    })
+    }
 
-    it('should deploy the test.sol contract', function() {
-      chai.assert.equal(testInstance.sender(), accts[0])
-      chai.assert.equal(testInstance.addr(), accts[1])
-      chai.assert.equal(testInstance.number().c[0], 4)
-      chai.assert.equal(testInstance.boolean(), true)
-    })
-    it('should capture the address, abi, txHash and bytecode of the test.sol contract',
-        function() {
-      chai.assert.typeOf(output.Test.address, 'string')
-      chai.assert.typeOf(output.Test.abi, 'array')
-      chai.assert.typeOf(output.Test.txHash, 'string')
-      chai.assert.typeOf(output.Test.bytecode, 'string')
 
+    describe('#compileAndDeploy(opts)', function() {
+
+      before(function(done) {
+        setup(done)
+      })
+
+      it('should deploy the test.sol contract', function() {
+        chai.assert.equal(testInstance.sender(), accts[0])
+        chai.assert.equal(testInstance.addr(), accts[1])
+        chai.assert.equal(testInstance.number().c[0], 4)
+        chai.assert.equal(testInstance.boolean(), true)
+      })
+      it('should capture the address, abi, txHash and bytecode of the test.sol contract',
+          function() {
+        chai.assert.typeOf(output.Test.address, 'string')
+        chai.assert.typeOf(output.Test.abi, 'array')
+        chai.assert.typeOf(output.Test.txHash, 'string')
+        chai.assert.typeOf(output.Test.bytecode, 'string')
+      })
+    })
+    describe('#compileAndDeployFromConfig(configPath)', function() {
+      it('should deploy the test.sol contract')
+      it('should capture the address, abi, txHash and bytecode of the test.sol contract')
+    })
+    describe('#writeOutput(path, output)', function() {
+      it('should write the address, abi, txHash and bytecode of the test.sol contract')
+      it('should do so when the path already exists')
+      it('should do so when the path does not exist')
     })
   })
-  describe('#compileAndDeployFromConfig(configPath)', function() {
-    it('should deploy the test.sol contract')
-  })
-  describe('#writeOutput(path, output)', function() {
 
+  describe('using a single contract with dependencies', function() {
+    describe('#compileAndDeploy(opts)', function() {
+      it('should deploy the testDeps.sol contract')
+      it('should capture the address, abi, txHash and bytecode of the testDeps.sol contract')
+    })
+    describe('#compileAndDeployFromConfig(configPath)', function() {
+      it('should deploy the testDeps.sol contract')
+      it('should capture the address, abi, txHash and bytecode of the testDeps.sol contract')
+    })
+    describe('#writeOutput(path, output)', function() {
+      it('should write the address, abi, txHash and bytecode of the testDeps.sol contract')
+      it('should do so when the path already exists')
+      it('should do so when the path does not exist')
+    })
+  })
+  describe('using multiple contracts without dependencies', function() {
+    describe('#compileAndDeploy(opts)', function() {
+      it('should deploy the multiTest.sol contract')
+      it('should capture the address, abi, txHash and bytecode of the multiTest.sol contract')
+    })
+    describe('#compileAndDeployFromConfig(configPath)', function() {
+      it('should deploy the multiTest.sol contract')
+      it('should capture the address, abi, txHash and bytecode of the multiTest.sol contract')
+    })
+    describe('#writeOutput(path, output)', function() {
+      it('should write the address, abi, txHash and bytecode of the multiTest.sol contract')
+      it('should do so when the path already exists')
+      it('should do so when the path does not exist')
+    })
+  })
+  describe('using multiple contracts with dependencies', function() {
+    describe('#compileAndDeploy(opts)', function() {
+      it('should deploy the multiTestDeps.sol contract')
+      it('should capture the address, abi, txHash and bytecode of the multiTestDeps.sol contract')
+    })
+    describe('#compileAndDeployFromConfig(configPath)', function() {
+      it('should deploy the multiTestDeps.sol contract')
+      it('should capture the address, abi, txHash and bytecode of the est.sol contract')
+    })
+    describe('#writeOutput(path, output)', function() {
+      it('should write the address, abi, txHash and bytecode of the multiTestDeps.sol contract')
+      it('should do so when the path already exists')
+      it('should do so when the path does not exist')
+    })
   })
 })
